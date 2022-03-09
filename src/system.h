b390742d8d827e66566ede71faaea83c5b56100b
Predators <31931530+predators46@users.noreply.github.com>
2022-03-09 14:25:16 +0800

    Add files via upload

--- src/system.h
+++ src/system.h
@@ -77,7 +77,6 @@
 #endif
 
 #ifdef HAVE_SYS_SYSINFO_H
-#define _LINUX_SYSINFO_H
 #include <sys/sysinfo.h>
 #endif
 
@@ -162,6 +161,10 @@
 
 #ifdef HAVE_NET_IF_TUN_H
 #include <net/if_tun.h>
+#endif
+
+#ifdef HAVE_NET_ETHERNET_H
+#include <net/ethernet.h>
 #endif
 
 #ifdef HAVE_ASM_TYPES_H
